faed6391e25b0af8663236952795628336e0c275
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-12 14:14:12 -0300

    feat(design-system): adiciona tokens de cores e fontsize

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,7 @@
 import { themes } from '@storybook/theming';
 
+import '../src/styles/global.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -11,4 +13,4 @@ export const parameters = {
   docs: {
     theme: themes.dark,
   },
-}
+} 
