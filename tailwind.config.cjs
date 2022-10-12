cc64e56dbc754b6e5dac3e8c552c4266893ab1ef
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-12 14:57:41 -0300

    feat(design-system): adiciona cores black e white

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,9 @@ module.exports = {
     },
     
     color: {
+      black: '#000',
+      white: '#FFF',
+      
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7c7c8a',
