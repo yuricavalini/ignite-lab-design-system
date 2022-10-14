fbdd4c9e33737178400e6bf0ebf6beb20607f500
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-14 12:34:01 -0300

    test(signin-page): garante que a mensagem de sucesso seja exibida ao submeter o formulario

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 import { SignIn } from './SignIn';
 
 export default {
@@ -8,5 +10,17 @@ export default {
   argTypes: {},
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
 
+    userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'yuri@exemplo.com.br')
+    userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
+
+    userEvent.click(canvas.getByRole('button', { name: 'Entrar na plataform' }))
+
+    await waitFor(() => {
+      expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+    })
+  }
+}
